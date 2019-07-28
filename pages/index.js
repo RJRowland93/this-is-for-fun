2415be1709ea87d65d848b84d29c541baccc9530
Josh Rowland <r.rowland93@gmail.com>
2019-07-28 16:07:40 -0500

    Set up initial graphql server

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
-const Index = () => {
+import axios from "axios";
+
+const Index = props => {
+  const { users } = props;
   return (
     <div
       style={{
         height: "600px",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        flexDirection: "column"
       }}
     >
       <p>Real dank tomfooler coming soon...</p>
+      <p>{`Coming to you from ${users.map(({ name }) => name)}`}</p>
     </div>
   );
+};
+
+Index.getInitialProps = async ({ req }) => {
+  const {
+    data: {
+      data: { users }
+    }
+  } = await axios.post("http://localhost:3000/api/graphql", {
+    query: `{ users { name } }`
+  });
+  return { users };
 };
 
 export default Index;

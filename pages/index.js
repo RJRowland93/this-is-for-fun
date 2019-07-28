1dedf23c586cb1f9a15f02ec775e39ba4e197234
Randal Rowland <r.rowland93@gmail.com>
2019-07-28 15:05:24 -0500

    Initial Nextjs project setup

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+const Index = () => {
+  return (
+    <div
+      style={{
+        height: "600px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <p>Real dank tomfooler coming soon...</p>
+    </div>
+  );
+};
+
+export default Index;
